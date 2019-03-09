7c8f09e1d106510307e3a35a2ff3286385d3a369
warlordjones <warlordjones@gmail.com>
2019-03-09 22:25:43 +0000

    Save BEFORE committing dumbass

--- src/main/java/xyz/brassgoggledcoders/adef/ADEF.java
+++ src/main/java/xyz/brassgoggledcoders/adef/ADEF.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.GameType;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,18 +13,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ADEF {
 	@SubscribeEvent
 	public static void onPlayerClone(EntityTravelToDimensionEvent event) {
-		FMLLog.warning("1");
 		if(event.getEntity().dimension == 1 && event.getDimension() == 1) {
-			FMLLog.warning("1.25");
 			if(event.getEntity() instanceof EntityPlayer) {
-				FMLLog.warning("1.4");
 				EntityPlayer player = (EntityPlayer) event.getEntity();
 				if(player.isServerWorld() && player instanceof EntityPlayerMP) {
-					FMLLog.warning("1.5");
-					// if(!event.isWasDeath()) {
-					FMLLog.warning("2");
 					if(!player.capabilities.isCreativeMode) {
-						FMLLog.warning("3");
 						player.setGameType(GameType.SURVIVAL);
 					}
 
